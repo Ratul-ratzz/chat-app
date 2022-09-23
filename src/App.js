5885ebf5ef4f6912b4202d943204090cf47ee38c
unknown <ratul.officialid@gmail.com>
2022-09-23 11:41:44 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-ratul.herokuapp.com/");
+// http://localhost:3001
 
 function App() {
   const [username, setUsername] = useState("");
